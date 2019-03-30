e7fba660757bd833f6fc3947d0bb86a45b790ec2
meneghetti1512 <meneghetti1512@gmail.com>
2019-03-30 16:53:42 -0400

    Fixing_Layout

--- template_engine.py
+++ template_engine.py
@@ -9,7 +9,7 @@ event1 = {
 	"title": "NDConomics Dynamic", 
 	"tags": ["Economics", "Comedy", "Physics"], 
 	"description": "Round table with economists Henrique Meirelles, Paulo Guedes, Milton Friedman and special guest Vitor Furtado, world champion in saying garbage.", 
-	"date": "Friday, 6PM", 
+	"date": "Friday, 6:00PM", 
 	"location": "141 DeBartolo Hall"
 }
 
@@ -17,27 +17,35 @@ event2 = {
 	"title": "Alan Turing at ND", 
 	"tags": ["Mathematics", "Machine Learning"], 
 	"description": "Join Alan Turing on an interesting lecture about the future of Machine Learning.", 
-	"date": "Wednesday, 2PM", 
+	"date": "Wednesday, 2:00PM", 
 	"location": "B19 - Fitzpatrick Hall of Engineering"
 }
 
 event3 = {
 	"title": "Event 3", 
 	"tags": ["Tag 1", "Tag 2"], 
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt. Nam lacinia turpis non faucibus tincidunt. Proin volutpat, nisi sit amet rutrum euismod, dui est maximus massa, in pharetra mauris erat pharetra erat.", 
-	"date": "Thursday, 3PM", 
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.", 
+	"date": "Thursday, 3:00PM", 
 	"location": "Jenkins Hall"
 }
 
 event4= {
 	"title": "Event 4", 
 	"tags": ["Tag 1", "Tag 2"], 
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt. Nam lacinia turpis non faucibus tincidunt. Proin volutpat, nisi sit amet rutrum euismod, dui est maximus massa, in pharetra mauris erat pharetra erat.", 
-	"date": "Thursday, 3PM", 
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.", 
+	"date": "Thursday, 3:00PM", 
 	"location": "Jenkins Hall"
 }
 
-events = [event1, event2, event3, event4];
+event5 = {
+	"title": "Rear Window (1954)",
+	"tags": ["Movies"],
+	"description": "Laid up with a broken leg, professional photographer L.B. “Jeff” Jefferies (James Stewart) combats the cabin fever swelling about...",
+	"date": "Wednesday, 7:30PM",
+	"location": "Browning Cinema, DeBartolo Performing Arts Center"
+}
+
+events = [event1, event2, event3, event4, event5];
 
 output = template.render(events=events)
 with open('landing_dynamic.html', 'w') as f:
