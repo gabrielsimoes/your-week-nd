84f933126b7b94136b93bc468bd198ad0112a790
meneghetti1512 <meneghetti1512@gmail.com>
2019-03-31 03:02:42 -0400

    Fixing Dates

--- template_engine.py
+++ template_engine.py
@@ -2,6 +2,28 @@ from jinja2 import Environment, FileSystemLoader
 import json
 from datetime import datetime
 
+def FormatDate(event):
+	if len(event["whole_days"]) > 0:
+		start_date = datetime.strptime(event["whole_days"][0], '%Y-%m-%d')
+		end_date = datetime.strptime(event["whole_days"][-1], '%Y-%m-%d')
+		if len(event["partial_days"]) > 0:
+			start_time = event["partial_days"][0]["start"][:-3]
+			end_time = event["partial_days"][0]["end"]
+			event["formatted_date"] = start_date.strftime("%m/%d") + " - " + end_date.strftime("%m/%d") + ", " + start_time + "-" + end_time
+		else:
+			event["formatted_date"] = start_date.strftime("%m/%d") + " - " + end_date.strftime("%m/%d")
+
+	elif len(event["partial_days"]) > 0:
+		weekday = datetime.strptime(event["partial_days"][0]["day"] , '%Y-%m-%d')
+		weekday = weekday.strftime('%A')
+		start_time = event["partial_days"][0]["start"][:-3]
+		end_time = event["partial_days"][0]["end"]
+		event["formatted_date"] = weekday + ", " + start_time + "-" + end_time
+	
+	else:
+		event["formatted_date"] = "See Details"
+	return event
+
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 
@@ -15,14 +37,11 @@ with open('events.json') as json_file:
 	# 	print(event["title"])
 	event_array = []
 	for name, event in events.items():
-		if len(event["partial_days"]) > 0:
-			weekday = datetime.strptime(event["partial_days"][0]["day"] , '%Y-%m-%d')
-			weekday = weekday.strftime('%A')
-			start_time = event["partial_days"][0]["start"][:-3]
-			end_time = event["partial_days"][0]["end"]
-			event["formatted_date"] = weekday + ", " + start_time + "-" + end_time
+		event = FormatDate(event)
 		event_array.append(event)
-	print(event_array[1]["partial_days"][0]["day"])
+
 output = template.render(events=event_array)
 with open('landing_dynamic.html', 'w') as f:
 	print(output, file=f)
+
+
