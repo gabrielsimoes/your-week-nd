53f29e89177f6a52d385a2a6eac0ca1b1e2432f9
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2019-03-31 00:55:22 -0400

    Implement user management

---
+++ users.py
@@ -0,0 +1,35 @@
+import pickle
+import bcrypt
+
+def load_users():
+    try:
+        users_file = open("users.pickle", "rb")
+        return pickle.load(users_file)
+    except FileNotFoundError:
+        return {}
+        pass
+
+def save_users(users_db):
+    with open("users.pickle", "wb") as out_file:
+        pickle.dump(users_db, out_file, pickle.HIGHEST_PROTOCOL)
+
+def user_exists(username):
+    users_db = load_users()
+    if username in users_db:
+        return True
+    else:
+        return False
+
+def add_user(username, password):
+    users_db = load_users()
+    if not user_exists(username):
+        users_db[username] = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        save_users(users_db)
+
+def check_user(username, password):
+    users_db = load_users()
+    if not user_exists(username):
+        return False
+    else:
+        return bcrypt.checkpw(password.encode('utf8'), users_db[username])
+
