54955a14e2f6c0c4e5a6fdbbf17cab2006e31383
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2019-03-31 01:00:14 -0400

    Implement basic template serving into server

--- template_engine.py
+++ template_engine.py
@@ -1,39 +1,32 @@
-from jinja2 import Environment, FileSystemLoader
-
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader)
-
-template = env.get_template('landing.html')
-
 event1 = {
-	"title": "NDConomics Dynamic", 
-	"tags": ["Economics", "Comedy", "Physics"], 
-	"description": "Round table with economists Henrique Meirelles, Paulo Guedes, Milton Friedman and special guest Vitor Furtado, world champion in saying garbage.", 
-	"date": "Friday, 6:00PM", 
+	"title": "NDConomics Dynamic",
+	"tags": ["Economics", "Comedy", "Physics"],
+	"description": "Round table with economists Henrique Meirelles, Paulo Guedes, Milton Friedman and special guest Vitor Furtado, world champion in saying garbage.",
+	"date": "Friday, 6:00PM",
 	"location": "141 DeBartolo Hall"
 }
 
 event2 = {
-	"title": "Alan Turing at ND", 
-	"tags": ["Mathematics", "Machine Learning"], 
-	"description": "Join Alan Turing on an interesting lecture about the future of Machine Learning.", 
-	"date": "Wednesday, 2:00PM", 
+	"title": "Alan Turing at ND",
+	"tags": ["Mathematics", "Machine Learning"],
+	"description": "Join Alan Turing on an interesting lecture about the future of Machine Learning.",
+	"date": "Wednesday, 2:00PM",
 	"location": "B19 - Fitzpatrick Hall of Engineering"
 }
 
 event3 = {
-	"title": "Event 3", 
-	"tags": ["Tag 1", "Tag 2"], 
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.", 
-	"date": "Thursday, 3:00PM", 
+	"title": "Event 3",
+	"tags": ["Tag 1", "Tag 2"],
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.",
+	"date": "Thursday, 3:00PM",
 	"location": "Jenkins Hall"
 }
 
 event4= {
-	"title": "Event 4", 
-	"tags": ["Tag 1", "Tag 2"], 
-	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.", 
-	"date": "Thursday, 3:00PM", 
+	"title": "Event 4",
+	"tags": ["Tag 1", "Tag 2"],
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in justo libero. Suspendisse nec dolor et enim iaculis tincidunt.",
+	"date": "Thursday, 3:00PM",
 	"location": "Jenkins Hall"
 }
 
@@ -47,6 +40,3 @@ event5 = {
 
 events = [event1, event2, event3, event4, event5];
 
-output = template.render(events=events)
-with open('landing_dynamic.html', 'w') as f:
-	print(output, file=f)
